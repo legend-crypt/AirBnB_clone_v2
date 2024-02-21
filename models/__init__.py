8c66f845d237850c82d3719f6efd3cffa0f7e23a
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-21 17:50:51 +0000

    Db storage and file storage fixed

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
+import os
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
 
-storage = FileStorage()
+storage_type = os.environ.get("HBNB_TYPE_STORAGE")
+
+if storage_type == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
+
 storage.reload()
