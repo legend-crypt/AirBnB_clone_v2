7ff583f5db971c67cd0d7acf2964c0cc5f7ddb67
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-22 18:32:29 +0000

    Places imported

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.place import Place
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
 from os import getenv
