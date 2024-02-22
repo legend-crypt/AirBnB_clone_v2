406e0437196ae225b99c4354232964ad76889421
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-22 18:09:47 +0000

    user.py

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from os import getenv
 
 
@@ -14,6 +15,9 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
+
+        places = relationship("Place", backref="user",
+                              cascade="all, delete-orphan")
     else:
         email = ''
         password = ''
