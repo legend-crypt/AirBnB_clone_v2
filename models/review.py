40e24c8426a54e7e34bbeff7f21f1d490a76f93b
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-22 18:25:13 +0000

    Review added to check for database

--- models/review.py
+++ models/review.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from os import getenv
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "reviews"
+
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), nullable=False, ForeignKey('places.id'))
+        user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
