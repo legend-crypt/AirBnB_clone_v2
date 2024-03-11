96e8fd676abec140ca318d95196eb165e06b6982
lord <konadulordkweku@gmail.com>
2024-03-11 16:27:50 +0000

    modify bash file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
-#script that sets up your web servers for the deployment
+# a script to install and conf nginx
 
-apt-get install nginx -y
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/releases/test/
-echo '<h1>Hello World</h1>' > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-sed -i '16a\
-  location /hbnb_static/ {\
-	alias /data/web_static/current/;\
- }' /etc/nginx/sites-available/default
-# sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+# script installs nginx
+sudo apt-get update
+sudo apt-get install nginx -y
 
+#create repositories
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+#dummy html page created
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+#create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#change directory name
+sudo chown -R ubuntu:ubuntu /data/
+
+#set up the page to be served
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+
+#restart the server
 sudo service nginx restart
