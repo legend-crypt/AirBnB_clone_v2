34f109b627aa04040843fcb12984d0ac3ab76eb6
lord <konadulordkweku@gmail.com>
2024-03-27 22:23:33 +0000

    fix errors

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""import modules to handle wsgi"""
+"""
+Starts a Flask web application
+"""
+
 from flask import Flask, render_template
+from models import *
 from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def list_cities():
-    """render html page that lists out states"""
-    states = storage.all("State")
-    sorted_states = sorted(list(states.values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
