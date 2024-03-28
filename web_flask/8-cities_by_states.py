5813c27243ad91356ce2dd20836af9aa4f6883d7
lord <konadulordkweku@gmail.com>
2024-03-28 00:45:51 +0000

    fix styling error

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,10 +13,12 @@ def city_by_state():
     states = storage.all("State").values()
     return render_template("8-cities_by_states.html", states=states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
